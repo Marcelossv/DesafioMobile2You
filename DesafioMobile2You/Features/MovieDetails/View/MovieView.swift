abcb517fa7973279885da25ebda16d94ef906596
Marcelo Silva <marcelos89@icloud.com>
2022-07-21 16:54:42 -0300

    created movie View

--- DesafioMobile2You/Features/MovieDetails/View/MovieView.swift
+++ DesafioMobile2You/Features/MovieDetails/View/MovieView.swift
@@ -8,5 +8,40 @@
 import UIKit
 
 class MovieView: UIView {
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .red
+//        tableView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellReuseIdentifier: <#T##String#>)
+        return tableView
+    }()
+    
+//MARK: - Initializers
+    
+    override init( frame: CGRect) {
+        super.init(frame: frame)
 
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//MARK: - Private Functions
+    
+    private func addElements(){
+        self.addSubview(self.tableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+        
+            self.tableView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            self.tableView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            self.tableView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor)
+
+        ])
+    }
 }
