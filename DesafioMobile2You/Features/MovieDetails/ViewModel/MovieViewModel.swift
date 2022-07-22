4e82c9795bcedb5a4e03ac2bd280e482abb39f51
Marcelo Silva <marcelos89@icloud.com>
2022-07-22 17:29:06 -0300

    ViewModel Logic request

--- DesafioMobile2You/Features/MovieDetails/ViewModel/MovieViewModel.swift
+++ DesafioMobile2You/Features/MovieDetails/ViewModel/MovieViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+protocol MovieViewModelDelegate: AnyObject {
+    func success()
+    func error()
+}
+
+class MovieViewModel{
+    
+    private let service: MovieService = MovieService()
+    private weak var delegate: MovieViewModelDelegate?
+    private var movieDetails: MovieDetails?
+    
+    
+    public func delegate (delegate:MovieViewModelDelegate?){
+        self.delegate = delegate
+    }
+    
+    public func getMovieDetailsRequest(){
+        self.service.getMovieDetails { success, error in
+            if let success = success {
+                self.movieDetails = success
+                self.delegate?.success()
+            } else {
+                self.delegate?.error()
+            }
+        }
+    }
+    
+}
